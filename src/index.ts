0895e944af5f3039d5a09338e35bf1d7bc21511a
John Lieb-Bauman <jbaum012@gmail.com>
2022-10-04 15:01:25 -0500

    make running this example easier

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {
   UsVerificationsWritable
 } from "@lob/lob-typescript-sdk";
 import { testData } from './data';
-const API_KEY = process.env.API_KEY;
+const API_KEY = "<YOUR API KEY>";
 
 export async function example() {
   const config: Configuration = new Configuration({
@@ -15,8 +15,15 @@ export async function example() {
   const usVerificationsClient = new UsVerificationsApi(config);
   const lobWritables = testData.map(item => new UsVerificationsWritable(item));
   const lobAddresses = new MultipleComponentsList({ addresses: lobWritables });
-  const results = await usVerificationsClient.verifyBulk(lobAddresses);
-  return results;
+  try {
+    const results = await usVerificationsClient.verifyBulk(lobAddresses);
+    console.log('results:', results);
+    return results;
+  } catch (err:any) {
+    console.error(err.message);
+    throw new Error(err);
+  }
+
 }
 
 example();
